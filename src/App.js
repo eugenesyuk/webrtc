333edc833578046e69da6a472bba47b0875e1ad1
Eugene Syrotiuk <eugenesyuk@gmail.com>
2022-07-10 23:45:56 +0200

    Setup eslint, prettier, format code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import "./App.scss";
+import './App.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <main className="App-container">Silence is golden</main>
-    </div>
-  );
+    return (
+        <div className="App">
+            <main className="App-container">Silence is golden</main>
+        </div>
+    )
 }
 
-export default App;
+export default App
