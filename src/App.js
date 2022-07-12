911a7bd49a75f8a8f5e1e7449d9b27f59533f464
Eugene Syrotiuk <eugenesyuk@gmail.com>
2022-07-12 23:18:56 +0200

    Add Material UI, Home template, reorganize files with folders

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.scss'
-import { peerConnection } from './Connection'
-import { firebase, firestore } from './Firebase'
+
+import { ThemeProvider } from '@mui/material/styles'
+import DefaultTheme from './DefaultTheme'
+
+import { firebase, firestore } from './core/Firebase'
+import { peerConnection } from './core/Connection'
+
+import Home from './components/Home'
 
 function App() {
     console.log({ firebase, firestore })
     console.log(peerConnection)
 
     return (
-        <div className="App">
-            <main className="App-container">Silence is golden</main>
-        </div>
+        <ThemeProvider theme={DefaultTheme}>
+            <Home />
+        </ThemeProvider>
     )
 }
 
---
+++ src/DefaultTheme.js
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material'
+
+const DefaultTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#3399ff'
+        },
+        mode: 'dark'
+    }
+})
+
+export default DefaultTheme
