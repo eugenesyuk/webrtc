3c33a3134bb2a51c767533252ce56c866f6a5d64
Eugene Syrotiuk <eugenesyuk@gmail.com>
2022-07-13 00:57:28 +0200

    Setup theme color scheme and global settings

--- src/DefaultTheme.js
+++ src/DefaultTheme.js
@@ -2,10 +2,52 @@ import { createTheme } from '@mui/material'
 
 const DefaultTheme = createTheme({
     palette: {
+        mode: 'dark',
         primary: {
-            main: '#3399ff'
+            main: '#3399ff',
+            contrastText: '#fff'
         },
-        mode: 'dark'
+        background: {
+            paper: '#0a1929',
+            default: '#001e3c'
+        },
+        grey: {
+            700: '#3e5060'
+        },
+        text: {
+            primary: '#fff'
+        }
+    },
+    typography: {
+        h5: {
+            color: '#66b2ff'
+        },
+        button: {
+            textTransform: 'initial'
+        }
+    },
+    shape: {
+        borderRadius: '8px'
+    },
+    components: {
+        MuiLink: {
+            defaultProps: {
+                underline: 'none',
+                textTransform: 'none'
+            },
+            styleOverrides: {
+                root: {
+                    ':hover': {
+                        color: '#99ccf3'
+                    }
+                }
+            }
+        },
+        MuiButton: {
+            defaultProps: {
+                disableElevation: true
+            }
+        }
     }
 })
 
